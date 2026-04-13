b3cccd258312e11e63799701815c944ac3f206fc
ruftnishida0927-cmd <ruft.nishida.0927@gmail.com>
2026-04-13 15:23:18 +0900

    Create staff_contact.py

---
+++ staff_contact.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def get_staff_contact(name):
+
+    contact = {
+        "看護部長": "調査中",
+        "事務長": "調査中",
+        "人事担当": "調査中",
+        "代表電話": "調査中",
+        "採用窓口": "総務課"
+    }
+
+    return contact
