3a558c46de762b7550eb5f181a90eeafc8191809
ruftnishida0927-cmd <ruft.nishida.0927@gmail.com>
2026-04-15 14:28:34 +0900

    Update staff_contact.py

--- staff_contact.py
+++ staff_contact.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
-def get_staff_contact(name):
-
+def get_staff_contact_debug(hospital_name: str, area: str = "", hospital_info: dict | None = None):
     contact = {
-        "看護部長": "調査中",
-        "事務長": "調査中",
-        "人事担当": "調査中",
-        "代表電話": "調査中",
-        "採用窓口": "総務課"
+        "看護部長": "不明",
+        "事務長": "不明",
+        "人事担当": "不明",
+        "代表電話": "不明",
+        "採用窓口": "不明",
+        "URL": "",
+        "スコア": 0
     }
 
+    debug = {
+        "candidate_url_count": 0,
+        "page_details": []
+    }
+
+    return contact, debug
+
+
+def get_staff_contact(hospital_name: str, area: str = "", hospital_info: dict | None = None):
+    contact, _ = get_staff_contact_debug(hospital_name, area, hospital_info)
     return contact
